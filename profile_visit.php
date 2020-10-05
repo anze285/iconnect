1c7965d13bb6587a0860c0708c3cf6a559652919
anze285 <anze285@users.noreply.github.com>
2020-10-05 20:03:02 +0200

    followers pop up

--- profile_visit.php
+++ profile_visit.php
@@ -71,14 +71,86 @@ if (empty($user1['profile_pic'])) {
                 $stmt->execute([$_SESSION['profile_id']]);
                 $followers = $stmt->fetch();
                 ?>
-                <span class="fs-profile"><span class="font-weight-bolder"><?php echo $followers["count"]; ?></span><span class="mr-1 mr-sm-3"> followers</span></span>
+                <span type="button" data-toggle="modal" data-target="#modalfollowers" class="fs-profile"><span class="font-weight-bolder"><?php echo $followers["count"]; ?></span><span class="mr-1 mr-sm-3"> followers</span></span>
+                <div class="modal fade" id="modalfollowers" data-backdrop="static" data-keyboard="false" tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
+                    <div class="modal-dialog modal-dialog-scrollable modal-dialog-centered">
+                        <div class="modal-content">
+                            <div class="modal-header">
+                                <h5 class="modal-title">Followers</h5>
+                                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                                    <span aria-hidden="true">&times;</span>
+                                </button>
+                            </div>
+                            <div class="modal-body">
+                                <?php
+                                $query = "SELECT u.id AS id, u.username AS username, u.profile_pic AS profile_pic FROM users u INNER JOIN followers f ON u.id=f.follower_id WHERE f.user_id = ?";
+                                $stmt = $pdo->prepare($query);
+                                $stmt->execute([$_SESSION['profile_id']]);
+                                if($stmt->rowCount()==0){
+                                    echo $user['username']." doesn't have any followers yet.";
+                                }
+                                for ($i = 0; $followers = $stmt->fetch(); $i++) {
+                                    if (empty($followers['profile_pic'])) {
+                                        $followers['profile_pic'] = 'images/profile.png';
+                                    }
+                                    if ($i != 0) {
+                                        echo '<div class="dropdown-divider my-0"></div>';
+                                    }
+                                    echo '<form action="profile_session.php" method="POST">';
+                                    echo '<div role="button" onclick="this.parentNode.submit();" class="d-flex justify-content-start px-1 py-1 max-content"><div>';
+                                    echo '<img class="custom-img1 mr-3" src="' . $followers['profile_pic'] . '" alt="profile-pic">';
+                                    echo '</div><div class="my-auto pb-1"><span><span class="px-0 font-weight-bold py-0" href="">' . $followers['username'] . '</span></span></div></div>';
+                                    echo '<input type="number" name="id" value="' . $followers['id'] . '" hidden>';
+                                    echo '</form>';
+                                }
+                                ?>
+                            </div>
+                        </div>
+                    </div>
+                </div>
                 <?php
                 $query = "SELECT COUNT(*) AS count FROM followers WHERE follower_id = ?";
                 $stmt = $pdo->prepare($query);
                 $stmt->execute([$_SESSION['profile_id']]);
                 $following = $stmt->fetch();
                 ?>
-                <span class="fs-profile"><span class="font-weight-bolder"><?php echo $following["count"]; ?></span><span class="mr-1 mr-sm-3"> following</span></span>
+                <span type="button" data-toggle="modal" data-target="#modalfollowing" class="fs-profile"><span class="font-weight-bolder"><?php echo $following["count"]; ?></span><span> following</span></span>
+                <div class="modal fade" id="modalfollowing" data-backdrop="static" data-keyboard="false" tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
+                    <div class="modal-dialog modal-dialog-scrollable modal-dialog-centered">
+                        <div class="modal-content">
+                            <div class="modal-header">
+                                <h5 class="modal-title">Following</h5>
+                                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                                    <span aria-hidden="true">&times;</span>
+                                </button>
+                            </div>
+                            <div class="modal-body">
+                                <?php
+                                $query = "SELECT u.id AS id, u.username AS username, u.profile_pic AS profile_pic FROM users u INNER JOIN followers f ON u.id=f.user_id WHERE f.follower_id = ?";
+                                $stmt = $pdo->prepare($query);
+                                $stmt->execute([$_SESSION['profile_id']]);
+                                if ($stmt->rowCount() == 0) {
+                                    echo $user['username'] . " doesn't follow anyone yet.";
+                                }
+                                for ($i = 0; $following = $stmt->fetch(); $i++) {
+                                    if (empty($following['profile_pic'])) {
+                                        $following['profile_pic'] = 'images/profile.png';
+                                    }
+                                    if ($i != 0) {
+                                        echo '<div class="dropdown-divider my-0"></div>';
+                                    }
+                                    echo '<form action="profile_session.php" method="POST">';
+                                    echo '<div role="button" onclick="this.parentNode.submit();" class="d-flex justify-content-start px-1 py-1 max-content"><div>';
+                                    echo '<img class="custom-img1 mr-3" src="' . $following['profile_pic'] . '" alt="profile-pic">';
+                                    echo '</div><div class="my-auto pb-1"><span><span class="px-0 font-weight-bold py-0" href="">' . $following['username'] . '</span></span></div></div>';
+                                    echo '<input type="number" name="id" value="' . $following['id'] . '" hidden>';
+                                    echo '</form>';
+                                }
+                                ?>
+                            </div>
+                        </div>
+                    </div>
+                </div>
             </div>
             <div>
                 <span class="fs-profile font-weight-600"><?php echo $user['name']; ?></span>
